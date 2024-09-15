f3a73d0e13bcd512c01cc902c4715549cdc42b55
Rotcivosk <victormonte43@gmail.com>
2024-09-15 13:05:08 -0400

    upd move x and scale
    Adicionado movimentação horizontal, diminuir escala do player, e colocado assets placeholders

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,38 +6,58 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public bool isPlayerOne;
-    public string playerName;
-    public float playerSpeed;
-  
+    [SerializeField] bool isPlayerOne;
+    [SerializeField] float playerSpeed = 5f;
 
+
+    [SerializeField] private float minX = -5f; // Limite mínimo no eixo X
+    [SerializeField] private float maxX = 5f;  // Limite máximo no eixo X
+  
+    private Vector3 moveDirection; // Direção do movimento
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        bool isPressingUp = false;
-        bool isPressingDown = false;
-        
-        if (isPlayerOne) {
-            isPressingUp = Input.GetKey(KeyCode.W);
-            isPressingDown = Input.GetKey(KeyCode.S);
-        } else {
-            isPressingUp = Input.GetKey(KeyCode.UpArrow);
-            isPressingDown = Input.GetKey(KeyCode.DownArrow);
+        HandleInput();
+        MovePlayer();
+    }
+
+    // Método responsável por capturar as entradas de movimento
+    private void HandleInput()
+    {
+        float moveVertical = 0f;
+        float moveHorizontal = 0f;
+
+        if (isPlayerOne)
+        {
+            moveVertical = Input.GetKey(KeyCode.W) ? 1f : (Input.GetKey(KeyCode.S) ? -1f : 0f);
+            moveHorizontal = Input.GetKey(KeyCode.D) ? 1f : (Input.GetKey(KeyCode.A) ? -1f : 0f);
+        }
+        else
+        {
+            moveVertical = Input.GetKey(KeyCode.UpArrow) ? 1f : (Input.GetKey(KeyCode.DownArrow) ? -1f : 0f);
+            moveHorizontal = Input.GetKey(KeyCode.RightArrow) ? 1f : (Input.GetKey(KeyCode.LeftArrow) ? -1f : 0f);
         }
 
-        if (isPressingUp) {
-            transform.Translate(Vector3.up * playerSpeed * Time.deltaTime);
+        // Atualiza a direção do movimento
+        moveDirection = new Vector3(moveHorizontal, moveVertical, 0f).normalized;
+    }
 
-        }
+    // Método responsável por mover o jogador
+    private void MovePlayer()
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            transform.Translate(moveDirection * playerSpeed * Time.deltaTime);
 
-        if (isPressingDown) {
-            transform.Translate(Vector3.down * playerSpeed * Time.deltaTime);
+            // Limita a posição no eixo X após o movimento
+            float clampedX = Mathf.Clamp(transform.position.x, minX, maxX);
+            transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
         }
     }
 }
